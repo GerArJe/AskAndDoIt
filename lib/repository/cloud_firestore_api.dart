56a1c07e5182f3faebfe3adb5a6804699a3fc209
Germán Arevalo <germanarevalojerez@gmail.com>
2019-12-11 12:03:41 -0500

    bringing challenges from firebase

--- lib/repository/cloud_firestore_api.dart
+++ lib/repository/cloud_firestore_api.dart
@@ -1,9 +1,11 @@
 import 'package:ask_and_doit/model/Question.dart';
+import 'package:ask_and_doit/model/challenge.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:math';
 
 class CloudFirestoreAPI {
 
+  //bring questions and select one randomly
   String getRandomQuestion(List<DocumentSnapshot> questionsListSnapshot) {
     List<Question> questions = List<Question>();
 
@@ -18,5 +20,22 @@ class CloudFirestoreAPI {
     int index = random.nextInt(questions.length);
 
     return questions.elementAt(index).question;
+  }
+
+  //bring challenges and select one randomly
+  String getRandomChallenge(List<DocumentSnapshot> questionsListSnapshot) {
+    List<Challenge> challenges = List<Challenge>();
+
+    questionsListSnapshot.forEach((c) {
+      challenges.add(Challenge(
+          number: c.data['number'],
+          challenge: c.data['challenge']
+      ));
+    });
+
+    var random = new Random();
+    int index = random.nextInt(challenges.length);
+
+    return challenges.elementAt(index).challenge;
   }
 }
--- lib/ui/game_view.dart
+++ lib/ui/game_view.dart
@@ -1,12 +1,8 @@
-import 'dart:ffi';
-
-import 'package:ask_and_doit/model/Question.dart';
 import 'package:ask_and_doit/repository/cloud_firestore_api.dart';
 import 'package:ask_and_doit/widgets/button_deeppurple.dart';
 import 'package:ask_and_doit/widgets/gradient_back.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 class GameView extends StatefulWidget {
   static final nombrePagina = "GameView";
@@ -16,12 +12,12 @@ class GameView extends StatefulWidget {
 }
 
 class _GameViewState extends State<GameView> {
-  double screenWith;
+//  double screenWith;
   final Firestore _db = Firestore.instance;
 
   @override
   Widget build(BuildContext context) {
-    screenWith = MediaQuery.of(context).size.width;
+//    screenWith = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
@@ -36,31 +32,20 @@ class _GameViewState extends State<GameView> {
             children: <Widget>[
               Flexible(
                 child: Container(
-                    width: screenWith,
                     margin: EdgeInsets.only(
                         top: 10.0, left: 20.0, right: 20.0, bottom: 10.0),
                     child: randomQuestion()),
               ),
               Flexible(
                 child: Container(
-                  width: screenWith,
-                  child: Text(
-                    "Reto",
-                    style: TextStyle(
-                      fontSize: 37.0,
-                      fontFamily: "Lato",
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+                    margin: EdgeInsets.only(
+                        top: 10.0, left: 20.0, right: 20.0, bottom: 10.0),
+                    child: randomChallenge()),
               ),
               ButtonDeepPurple(
                   text: "Comenzar",
-                  onPressed: () =>
-                      Navigator.popAndPushNamed(context, GameView.nombrePagina)),
-
+                  onPressed: () => Navigator.popAndPushNamed(
+                      context, GameView.nombrePagina)),
             ],
           )
         ],
@@ -68,6 +53,7 @@ class _GameViewState extends State<GameView> {
     );
   }
 
+  //generate the StreamBuilder and execute the query of the questions
   StreamBuilder randomQuestion() {
     return StreamBuilder<QuerySnapshot>(
       stream: _db.collection('questions').snapshots(),
@@ -88,9 +74,45 @@ class _GameViewState extends State<GameView> {
     );
   }
 
-  Text textRandomQuestion(AsyncSnapshot<QuerySnapshot> snapshot){
+  //generate the StreamBuilder and execute the query of the Challenges
+  StreamBuilder randomChallenge() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _db.collection('challenges').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.done:
+            return textRandomChallenge(snapshot);
+          case ConnectionState.active:
+            return textRandomChallenge(snapshot);
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          default:
+            return textRandomChallenge(snapshot);
+        }
+      },
+    );
+  }
+
+  //generate the text of the questions
+  Text textRandomQuestion(AsyncSnapshot<QuerySnapshot> snapshot) {
     return Text(
       "Pregunta: ${CloudFirestoreAPI().getRandomQuestion(snapshot.data.documents)}",
+      style: TextStyle(
+        fontSize: 37.0,
+        fontFamily: "Lato",
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  //generate the text of the challenges
+  Text textRandomChallenge(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return Text(
+      "Reto: ${CloudFirestoreAPI().getRandomChallenge(snapshot.data.documents)}",
       style: TextStyle(
         fontSize: 37.0,
         fontFamily: "Lato",
